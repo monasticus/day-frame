977c5cfab88bf405b42f1a753fd673150dd502d3
aniolowt <tomasz.aniolowski@contractors.roche.com>
2021-03-25 15:56:27 +0100

    DF-2 - create BreakPointException class

---
+++ src/main/java/com/arch/dayframe/model/bp/BreakPointException.java
@@ -0,0 +1,47 @@
+package com.arch.dayframe.model.bp;
+
+public class BreakPointException extends Exception {
+
+    private ErrorCode errorCode;
+    private String errorSource;
+
+    public enum ErrorCode {
+        DESCRIPTION_ERR, TIME_ERR,
+        ALREADY_POSTPONED_ERR, ZERO_POSTPONE_ERR, NEGATIVE_POSTPONE_ERR
+    }
+
+    public BreakPointException(String message) {
+        super(message);
+    }
+
+    public BreakPointException(ErrorCode errorCode) {
+        this(errorCode, "");
+    }
+
+    public BreakPointException(ErrorCode errorCode, String errorSource) {
+        this.errorCode = errorCode;
+        this.errorSource = errorSource;
+    }
+
+    @Override
+    public String getMessage() {
+        switch (errorCode) {
+            case DESCRIPTION_ERR:
+                return String.format("Wrong break point description: %s", errorSource);
+            case TIME_ERR:
+                return String.format("Wrong break point time: %s", errorSource);
+            case ALREADY_POSTPONED_ERR:
+                return "Break point has been already postponed.";
+            case ZERO_POSTPONE_ERR:
+                return "Wrong postponement value: 0";
+            case NEGATIVE_POSTPONE_ERR:
+                return String.format("Wrong postponement value: %s", errorSource);
+            default:
+                return super.getMessage();
+        }
+    }
+
+    public ErrorCode getErrorCode(){
+        return errorCode;
+    }
+}
